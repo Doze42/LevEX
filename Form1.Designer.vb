9b425c84ef31e7ca4d846dc307e011586ede6cbc
Doze42 <62532206+Doze42@users.noreply.github.com>
2020-09-30 09:19:52 -0600

    Initial Upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtGameURL = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtXML = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SaveFileDialog = New System.Windows.Forms.SaveFileDialog()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(0, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(330, 50)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Sploder Raw Level Data Exporter" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(2, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(90, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Paste Game URL"
+        '
+        'txtGameURL
+        '
+        Me.txtGameURL.Location = New System.Drawing.Point(5, 96)
+        Me.txtGameURL.Name = "txtGameURL"
+        Me.txtGameURL.Size = New System.Drawing.Size(445, 20)
+        Me.txtGameURL.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(167, 122)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Exit"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(5, 122)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Get XML"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(2, 148)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Output:"
+        '
+        'txtXML
+        '
+        Me.txtXML.Location = New System.Drawing.Point(5, 164)
+        Me.txtXML.Multiline = True
+        Me.txtXML.Name = "txtXML"
+        Me.txtXML.ReadOnly = True
+        Me.txtXML.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtXML.Size = New System.Drawing.Size(445, 181)
+        Me.txtXML.TabIndex = 6
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(86, 122)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "Save XML"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'SaveFileDialog
+        '
+        Me.SaveFileDialog.Filter = "XML Document | *.xml"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(5, 46)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(249, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "By Alex Reiter @ 123computer.net / Doze42#4889"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(456, 352)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.txtXML)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtGameURL)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "LevEX"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtGameURL As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtXML As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents SaveFileDialog As SaveFileDialog
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+    Dim tempXML As String = System.IO.Path.GetTempFileName
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim temp1 As String = System.IO.Path.GetTempFileName
+        My.Computer.FileSystem.DeleteFile(temp1, FileIO.UIOption.OnlyErrorDialogs, FileIO.RecycleOption.DeletePermanently)
+        My.Computer.Network.DownloadFile(txtGameURL.Text, temp1)
+        Dim GameHTML As String = My.Computer.FileSystem.ReadAllText(temp1)
+        Dim sidx As Integer = GameHTML.IndexOf("s: ")
+        Dim cutS As String = GameHTML.Substring(sidx + 4, 8)
+        Dim temp2 As String = System.IO.Path.GetTempFileName
+        My.Computer.FileSystem.DeleteFile(temp2, FileIO.UIOption.OnlyErrorDialogs, FileIO.RecycleOption.DeletePermanently)
+        My.Computer.Network.DownloadFile("http://www.sploder.com/php/getgameprops.php?pubkey=" + cutS, temp2)
+        Dim gamevars As String = My.Computer.FileSystem.ReadAllText(temp2)
+        Dim varArray As String() = gamevars.Split("&")
+        Dim u As Integer = varArray(1).Substring(2, varArray(1).Length - 2)
+        Dim c As String = varArray(2).Substring(2, varArray(2).Length - 2)
+        Dim m As String = varArray(3).Substring(2, varArray(3).Length - 2)
+        My.Computer.FileSystem.DeleteFile(tempXML, FileIO.UIOption.OnlyErrorDialogs, FileIO.RecycleOption.DeletePermanently)
+        Dim url As String = "http://sploder.s3.amazonaws.com/users/group" & Math.Floor(u / 1000) & "/user" & u & "_" & c & "/projects/proj" & m & "/game.xml"
+        My.Computer.Network.DownloadFile(url, tempXML)
+        txtXML.Text = System.Xml.Linq.XDocument.Parse(My.Computer.FileSystem.ReadAllText(tempXML)).ToString()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        SaveFileDialog.ShowDialog()
+        My.Computer.FileSystem.CopyFile(tempXML, SaveFileDialog.FileName)
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+End Class
